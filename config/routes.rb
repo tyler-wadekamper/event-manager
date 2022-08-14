6b4efcb72bdb5fb9ee61d3e54ef50be0c5ba1a77
tyler-wadekamper <wadekampert@spu.edu>
2022-08-14 16:56:34 -0700

    Initialize devise resource for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :events, only: [:index, :new, :create]
