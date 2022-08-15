c05cc6a42f77b71a603cd0311e6b71e3df48c531
tyler-wadekamper <wadekampert@spu.edu>
2022-08-14 19:05:47 -0700

    Add show view for an event. Add organizer name to the _event partial.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :events, only: [:index, :new, :create]
+  resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
 
   # Defines the root path route ("/")
