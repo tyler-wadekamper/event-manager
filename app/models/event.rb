be6d6ce6b0b5ae578fbb7759e4330a568a440894
tyler-wadekamper <wadekampert@spu.edu>
2022-08-15 10:59:14 -0700

    Add past and upcoming scopes to event model. Add past/upcoming view separation for events#index view. Refactor users view for past/upcoming.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
   has_many :event_attendees, foreign_key: :event_id
   has_many :attendees, through: :event_attendees
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
